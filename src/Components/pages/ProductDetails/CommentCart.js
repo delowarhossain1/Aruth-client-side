5300c9481409a582fa53fa83b9d4b3800a6206e3
DeveloperDelowar <programmerdaloar@gmail.com>
2022-08-10 21:57:31 -0700

    product details page updated (order summery)

---
+++ src/Components/pages/ProductDetails/CommentCart.js
@@ -0,0 +1,35 @@
+import React from "react";
+import RatingsStar from "../../shared/Ratings/RatingsStar";
+
+const CommentCart = ({ comment }) => {
+  const { email, ratings, text } = comment;
+
+  return (
+    <div className="mb-5">
+      <div className="flex items-center mb-2">
+        <img
+          src="http://localhost:3000/static/media/male.697e97cdd2d0c4819b57.png"
+          alt="profile"
+          className="w-10 h-10 rounded-full border p-1"
+        />
+
+        <div className="ml-2 ">
+          <h2 className="mt-1">
+            {email.slice(0, 2)}*****
+            {email.slice(email.length - 6, email.length)}
+          </h2>
+          <h5 className="text-xs">
+            <i class="fa-solid fa-circle-check text-xs text-green-500 mr-1"></i>
+            Verified Purchase
+          </h5>
+        </div>
+      </div>
+
+      <RatingsStar star={ratings} styles="text-xs" />
+
+      <p className="mt-1">{text}</p>
+    </div>
+  );
+};
+
+export default CommentCart;
--- src/Components/pages/ProductDetails/ProductDetails.js
+++ src/Components/pages/ProductDetails/ProductDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import RatingsStar from "../../shared/Ratings/RatingsStar";
+import CommentCart from "./CommentCart";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -24,9 +25,8 @@ const ProductDetails = () => {
     description,
     comments,
   } = product;
-  console.log(description);
+
   const updateProductQuantity = (btn) => {
-    console.log(btn);
     if (btn === "plus" && productQuantity < availableQuantity) {
       setProductQuantity(productQuantity + 1);
     } else if (btn === "minus") {
@@ -38,7 +38,7 @@ const ProductDetails = () => {
 
   return (
     <section>
-      <div className="py-5 flex flex-col lg:flex-row">
+      <div className="py-5 flex flex-col lg:flex-row space-x-0 lg:space-x-5 space-y-5 lg:space-y-0">
         <div className="grid grid-cols-1 md:grid-cols-2 bg-white w-full md:w-8/12 rounded p-2">
           <div>
             <img src={img} alt="product" />
@@ -94,10 +94,41 @@ const ProductDetails = () => {
           </div>
         </div>
 
-        <div className="w-full md:w-4/12">dsfdsf</div>
+              {/* Orders summery */}
+
+        <div className="w-full md:w-4/12 p-3 bg-white shadow-md rounded">
+          <h4 className="text-xl mb-3 text-center">Summery</h4>
+
+          <div className="flex items-center justify-between mb-4">
+            <div className="flex items-center">
+              <i class="fa-solid fa-truck text-gray-500"></i>
+              <div className="flex flex-col ml-5">
+                <span>Standard Delivery</span>
+                <span className="text-sm text-gray-500">5 - 9 day(s)</span>
+              </div>
+            </div>
+
+            <h4 className="text-lg text-orange-500">$12</h4>
+          </div>
+
+          <div className="flex items-center mb-4">
+            <i class="fa-solid fa-money-bill-wave text-gray-500"></i>
+            <div className="flex flex-col ml-5">
+                <span>Cash on Delivery </span>
+                <span className="text-sm text-gray-500">Available</span>
+              </div>
+          </div>
+
+          <div className="flex items-center">
+            <i class="fa-solid fa-shield text-gray-500"></i>
+            <h2 className="ml-5">Warranty not available</h2>
+          </div>
+        </div>
       </div>
 
-      <div className="bg-white p-3 text-lg lg:text-xl mb-2">
+                {/* Product description & ratings */}
+                
+      <div className="bg-white p-3 mb-5">
         <h2 className="text-2xl mb-2">{name}</h2>
 
         <ul className="text-sm font-semibold list-disc ml-5 mb-5">
@@ -107,6 +138,27 @@ const ProductDetails = () => {
         </ul>
 
         <p className="text-sm">{description?.text}</p>
+      </div>
+
+      <div className="p-3 bg-white">
+        <h2 className="lg:text-xl">Ratings & Reviews of {name}</h2>
+
+        <div className="mb-10">
+          <h3 className="text-2xl lg:text-5xl text-orange-500 my-3">
+            {ratings}
+            <span className="text-xl text-gray-500">/5</span>
+          </h3>
+
+          <RatingsStar star={ratings} styles="text-xl lg:text-2xl" />
+
+          <h4 className="text-sm text-gray-500">21 Ratings</h4>
+        </div>
+
+        <div>
+          {comments?.map((comment) => (
+            <CommentCart key={comment._id} comment={comment} />
+          ))}
+        </div>
       </div>
     </section>
   );
