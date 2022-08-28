3338e7fe58608855531ff2eabb49fb32a6bb6095
DeveloperDelowar <programmerdaloar@gmail.com>
2022-08-28 12:42:21 -0700

    product card componenet updated

--- src/Components/pages/Home/Slider/Slider.js
+++ src/Components/pages/Home/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
--- src/Components/shared/Cart/ProductCart.js
+++ src/Components/shared/Cart/ProductCart.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import RatingsStar from './../Ratings/RatingsStar';
+import RatingsStar from "./../Ratings/RatingsStar";
 
-const ProductCart = ({product}) => {
-    const {_id, img, name, ratings, price, discount} = product;
-    const navigate = useNavigate();
+const ProductCart = ({ product }) => {
+  const { _id, img, name, ratings, price, discount } = product;
+  const navigate = useNavigate();
 
-    // Product name;
-    const productName = name?.length > 250 ? name.slice(0, 20) + "......" : name; 
+  // Product name;
+  const productName = name?.length > 250 ? name.slice(0, 20) + "......" : name;
 
   return (
-    <div className="bg-white cursor-pointer shadow rounded hover:shadow-lg" onClick={()=> navigate(`/product-details/${_id}`)}>
+    <div
+      className="bg-white cursor-pointer shadow rounded hover:shadow-lg"
+      onClick={() => navigate(`/product-details/${_id}`)}
+    >
       <img src={img} alt="product" />
 
       <div className="p-2 my-3">
-        <h2 className="text-lg">{productName}</h2>
+        <h2 className="text-lg">
+          {productName?.length > 35
+            ? productName?.slice(0, 35) + "..."
+            : productName}
+        </h2>
         <h3 className="text-2xl my-1">${price}</h3>
         <h4 className="text-sm text-gray-400 mb-2">
           <strike>${discount}</strike> - 65%
