2d12dd447d828179a89f9ab25f0de31e353f1bf7
DeveloperDelowar <programmerdaloar@gmail.com>
2022-09-13 10:36:57 -0700

    all category page created

--- src/Components/pages/Home/Categories/AllCategories.js
+++ src/Components/pages/Home/Categories/AllCategories.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "react-query";
+import CategoriesCart from "./CategoriesCart";
 
 const AllCategories = () => {
-    return (
-        <div>
-            hello 
-        </div>
-    );
+
+  const { data: categories, isLoading } = useQuery(["our-categories"], () =>
+    fetch(`http://localhost:5000/categories`).then((res) => res.json())
+  );
+
+  return (
+    <section className="py-5">
+      <h2 className="text-2xl text-gray-500">Our Categories</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+        {categories?.map((category) => (
+          <CategoriesCart key={category._id} category={category} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default AllCategories;
--- src/Components/pages/Home/Categories/CategoriesCart.js
+++ src/Components/pages/Home/Categories/CategoriesCart.js
@@ -5,7 +5,7 @@ const CategoriesCart = ({category}) => {
     const {link, img} = category; 
   return (
     <Link to={link}>
-      <div className="overflow-hidden">
+      <div className="overflow-hidden rounded">
         <img src={img} alt="category" className=" hover:scale-[1.07] duration-1000 max-w-[500px]" />
       </div>
     </Link>
